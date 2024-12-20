89c6a58604486206a64d4d337a1203477aff17a6
Agnik7 <bakshi.agnik@gmail.com>
2024-12-20 10:45:37 +0530

    feat: basic project setup

---
+++ arin/__init__.py
@@ -0,0 +1,5 @@
+# This is the import line
+from .utils.health import Health
+# End of import line
+allowed_classes = ["Health"] # List of all public facing classes
+__all__ = allowed_classes
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Arin",
+    version="0.1.0",
+    description="A one-stop machine learning solution",
+    author="Agnik Bakshi, Indranjana Chatterjee",
+    packages=find_packages(),
+)
