a05b1746d910ab914fc4b32b16a0ab7acbfb4f1e
Agnik7 <bakshi.agnik@gmail.com>
2024-12-21 09:19:07 +0530

    update:renamed to agin

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Arin",
+    name="Agin",
     version="0.1.0",
     description="A one-stop machine learning solution",
     author="Agnik Bakshi, Indranjana Chatterjee",
