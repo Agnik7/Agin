125123a9d534f80a05123bf9319b9523030d7416
Agnik7 <bakshi.agnik@gmail.com>
2024-12-30 17:58:41 +0530

    rebase

--- agin/classification/__init__.py
+++ agin/classification/__init__.py
@@ -6,6 +6,7 @@ from .nonlinear_svm import NonLinearSVM
 from .decision_tree_classifier import DecisionTreeClassifier
 from .linear_svm import LinearSVMClassifier
 from .nonlinear_svm import NonLinearSVM
+from .decision_tree_classifier import DecisionTreeClassifier
 allowed_classes = [
     "LogisticRegression",
     "NaiveBayesClassifier",
